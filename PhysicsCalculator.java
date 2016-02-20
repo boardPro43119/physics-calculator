deb22eea8a82f2c28c6a06d6fd4e897756b095e8
Benjamin Prud'homme <bgprudhomme@gmail.com>
2016-02-20 16:41:20 -0500

    Fix final velocity case, consolidate code

--- PhysicsCalculator.java
+++ PhysicsCalculator.java
@@ -1,10 +1,10 @@
 /**
-* Benjamin Prud'homme
-* February 13, 2016
-*
-* Solves for one value given several others from the following list:
-* displacement, initial velocity, final velocity, acceleration, and time.
-*/
+ * Benjamin Prud'homme
+ * Latest commit: February 20, 2016
+ *
+ * Solves for one value given several others from the following list:
+ * displacement, initial velocity, final velocity, acceleration, and time.
+ */
 
 import java.util.Scanner;
 public class PhysicsCalculator {
@@ -23,7 +23,7 @@ public class PhysicsCalculator {
 			System.out.println("Choose one of the following values to solve for: " + 
 							"displacement (m) , initial or final velocity (m/s), " +
 							"acceleration (m/s^2), or time (s).");
-			input = in.next();
+			input = in.nextLine();
 			invalidInput = false;
 			switch(input)
 			{
@@ -60,16 +60,12 @@ public class PhysicsCalculator {
 							initVelocity = in.nextDouble();
 							System.out.print("Time (s): ");
 							time = in.nextDouble();
+							in.nextLine(); //consume newline character from input stream
 							System.out.print("Do you know (1) acceleration or (2) final velocity? ");
 							
-							input = in.next();
+							input = in.nextLine();
 							switch(input){
 								case "1":
-									System.out.print("Acceleration (m/s^2): ");
-									acceleration = in.nextDouble();
-									displacement = (initVelocity*time)+(0.5*acceleration*time*time);
-									System.out.println("Displacement: " + displacement + " meters");
-									break;
 								case "acceleration":
 									System.out.print("Acceleration (m/s^2): ");
 									acceleration = in.nextDouble();
@@ -77,11 +73,6 @@ public class PhysicsCalculator {
 									System.out.println("Displacement: " + displacement + " meters");
 									break;
 								case "2":
-									System.out.print("Final velocity (m/s): ");
-									finalVelocity = in.nextDouble();
-									displacement = 0.5*(initVelocity+finalVelocity)*time;
-									System.out.println("Displacement: " + displacement + " meters");
-									break;
 								case "final velocity":
 									System.out.print("Final velocity (m/s): ");
 									finalVelocity = in.nextDouble();
@@ -89,6 +80,7 @@ public class PhysicsCalculator {
 									System.out.println("Displacement: " + displacement + " meters");
 									break;
 								default:
+									System.out.println("Invalid input, please try again.");
 									invalidInput=true;
 							}
 						} catch(RuntimeException e) {
